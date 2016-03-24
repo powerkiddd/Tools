c370af9f0077be7301d587b918dba9de73e0080e
Kevin Secret <powerkittt@gmail.com>
2016-03-24 22:26:56 +0100

    Update build number to correct version.

--- src/Main/Version.java
+++ src/Main/Version.java
@@ -1,6 +1,6 @@
 package Main;
 
 public class Version {
-	public static String version = "Alpha 0.0.0.10 (Development Version)";
+	public static String version = "Alpha 0.0.1.0 (Development Version)";
 	//public static String version = "Alpha 0.0.0.9";
 }
