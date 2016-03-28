a036d267667a27d4d09063d5cb7de7b419a7a2e9
Kevin Secret <powerkittt@gmail.com>
2016-03-28 19:34:26 +0200

    More work on Lighting

--- src/Game/World2.java
+++ src/Game/World2.java
@@ -550,13 +550,18 @@ public class World2 extends JPanel {
 			}
 			//Draw Lighting
 			/*Graphics2D g2d = (Graphics2D)g;
-			for (int i = 0; i < Video_Settings.window_size_y/Lighting.quality; i++) {
-				for (int j = 0; j < Video_Settings.window_size_x/Lighting.quality; j++) {
-					Composite translucent = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float) (Lighting.colors[i*j].getAlpha()/255f));
-					g2d.setComposite(translucent);
-					g2d.drawImage(Lighting.pixels[i*j], j*Lighting.quality, i*Lighting.quality, Lighting.quality, Lighting.quality, Lighting.colors[i*j], null);
-				}
+			Composite translucent = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float) (Lighting.daynight.getAlpha()/255f));
+			g2d.setComposite(translucent);
+			g2d.drawImage(Lighting.light, 0, 0, Video_Settings.window_size_x, Video_Settings.window_size_y, Lighting.daynight, null);
+			if (Lighting.daynight.getRed() > 0) {
+				Lighting.daynight = new Color(Lighting.daynight.getRed()-1, Lighting.daynight.getGreen()-1, Lighting.daynight.getBlue()-1, Lighting.daynight.getAlpha());
 			}
+			else if (Lighting.daynight.getAlpha() < 230) {
+				Lighting.daynight = new Color(0,0,0,Lighting.daynight.getAlpha()+1);
+			}
+			translucent = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, (float) (Lighting.torch.getAlpha()/255f));
+			g2d.setComposite(translucent);
+			g2d.drawImage(Lighting.light, 360, (int) (360-camera_y), 40, 40, Lighting.torch, null);
 			g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));*/
 			//Draw Hotbar
 			g.drawImage(invslot, f.getSize().width/2-116, 0, 29, 29, null);
--- src/Main/Lighting.java
+++ src/Main/Lighting.java
@@ -6,14 +6,14 @@ import java.io.File;
 
 import javax.imageio.ImageIO;
 
-import Settings.Video_Settings;
-
 public class Lighting {
 
 	public static BufferedImage[] pixels;
 	public static Color[] colors;
 	public static BufferedImage light = new BufferedImage(1,1,BufferedImage.TYPE_INT_ARGB);
+	public static Color daynight;
 	public static BufferedImage newlight;
+	public static Color torch;
 	public static int quality = 80;
 	
 	public static void main(String[] args) {
@@ -21,19 +21,12 @@ public class Lighting {
 			File file = new File("images\\light.png");
 			light = ImageIO.read(file);
 		} catch (Exception ex) {
-			
+			ex.printStackTrace();
+			System.exit(0);
 		}
-		//Fill pixels with standard info
-		pixels = new BufferedImage[(Video_Settings.window_size_x/quality) * (Video_Settings.window_size_y/quality)];
-		colors = new Color[(Video_Settings.window_size_x/quality) * (Video_Settings.window_size_y/quality)];
-		for (int i = 0; i < Video_Settings.window_size_y/quality; i++) {
-			for (int j = 0; j < Video_Settings.window_size_x/quality; j++) {
-				newlight = light;
-				Color c = new Color(255,255,255,40);
-				colors[i*j] = c;
-				pixels[i*j] = light;
-			}
-		}
+		
+		daynight = new Color(255,255,255,60);
+		torch = new Color(255,255,255,0);
 	}
 	
 }
