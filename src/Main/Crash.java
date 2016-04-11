8dfaf6f5b1392a23cecc10e2493452991c838d03
Kevin <k.wouda@applicationbuilders.nl>
2016-04-11 11:20:36 +0200

    Guess ;)

--- src/Main/Crash.java
+++ src/Main/Crash.java
@@ -7,6 +7,7 @@ public class Crash {
 	public static String cause = "Undefined, run the debugger (starter_debugger.bat) to be able to see all the errors!";
 	
 	public static void main(String[] args) {
+		SaveLoad.SaveGame();
 		String crashdialogue = "Well done, old chap! You crashed the game! \nDon't worry, whenever I implement a save game feature, your world will be automaticly saved and you can continue playing your game, afterward."
 				+ "\nBut for now, you crashed, the game.\n\nIf you would like to report this error, please send the following information to the creator of this game:"
 				+ "\nCause: " + cause;
