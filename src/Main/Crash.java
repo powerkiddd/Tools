12a8dd06c0b3ce0d7515efd44a29ee626fb50efe
Kevin <k.wouda@applicationbuilders.nl>
2016-04-11 11:23:56 +0200

    Update crash text, catch null.

--- src/Main/Crash.java
+++ src/Main/Crash.java
@@ -7,8 +7,11 @@ public class Crash {
 	public static String cause = "Undefined, run the debugger (starter_debugger.bat) to be able to see all the errors!";
 	
 	public static void main(String[] args) {
+		if (SaveLoad.worldname == null || SaveLoad.worldname == "") {
+			SaveLoad.worldname = "CrashSaveBackup";
+		}
 		SaveLoad.SaveGame();
-		String crashdialogue = "Well done, old chap! You crashed the game! \nDon't worry, whenever I implement a save game feature, your world will be automaticly saved and you can continue playing your game, afterward."
+		String crashdialogue = "Well done, old chap! You crashed the game! \nDon't worry, your game has been automaticly saved and you can continue playing your game at restart."
 				+ "\nBut for now, you crashed, the game.\n\nIf you would like to report this error, please send the following information to the creator of this game:"
 				+ "\nCause: " + cause;
 		JOptionPane.showMessageDialog(null, crashdialogue,"UH-OH", JOptionPane.ERROR_MESSAGE);
