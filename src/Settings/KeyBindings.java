1877e2956e821d5dfefd691c8e2bd3c85d216556
powerkiddd <powerkiddd@users.noreply.github.com>
2016-05-03 15:22:48 +0200

    Manually upload this file, because shit.

---
+++ src/Settings/KeyBindings.java
@@ -0,0 +1,10 @@
+package Settings;
+
+public class KeyBindings {
+	public static String walkleft = "a";
+	public static String walkright = "d";
+	public static String inventory = "i";
+	public static String snap = "Ctrl";
+	public static String run = "Shift";
+	public static String jump = "Space";
+}
