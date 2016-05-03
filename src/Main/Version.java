1ee2c1be9ed00154410c29ee9751d91e60ee3448
Kevin <k.wouda@applicationbuilders.nl>
2016-05-03 11:25:14 +0200

    Trying to keep up-to-date...

--- src/Main/Version.java
+++ src/Main/Version.java
@@ -1,6 +1,6 @@
 package Main;
 
 public class Version {
-	public static String version = "Alpha 0.0.1.0 (Development Version)";
-	//public static String version = "Alpha 0.0.0.9";
+	public static String version = "Alpha 0.0.1.1 (Development Version)";
+	//public static String version = "Alpha 0.0.1.0";
 }
