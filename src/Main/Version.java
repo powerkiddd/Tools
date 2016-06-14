a9e03152b0f9b4be49eff7631ce12e8ebb20fead
Kevin <k.wouda@applicationbuilders.nl>
2016-06-14 17:09:52 +0200

    Yesh

--- src/Main/Version.java
+++ src/Main/Version.java
@@ -1,6 +1,6 @@
 package Main;
 
 public class Version {
-	public static String version = "Alpha 0.0.1.1 (Development Version)";
-	//public static String version = "Alpha 0.0.1.0";
+	public static String version = "Alpha 0.0.1.2 (Development Version)";
+	//public static String version = "Alpha 0.0.1.1";
 }
